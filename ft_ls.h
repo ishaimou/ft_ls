a38a4a3bce3bb08401b6e9ea1ebbeb551be809c2
nayarix <ilyassukun@gmail.com>
2019-04-21 17:25:10 -0700

    add ft_ls header file

---
+++ ft_ls.h
@@ -0,0 +1,19 @@
+#ifndef FT_LS_H
+# define FT_LS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <dirent.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/xattr.h>
+# include <uuid/uuid.h>
+# include <pwd.h>
+# include <grp.h>
+# include <time.h>
+# include "libft.h"
+
+
+
+#endif
